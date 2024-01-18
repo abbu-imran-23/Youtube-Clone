f8ce062a460e8b1fc39c7d0177cf803d44cc2af8
Imran <abbuimran02@gmail.com>
2024-01-18 21:05:29 +0530

    youtube clone completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { AppContext } from './context/ContextApi'
+import Header from './components/Header';
+import Feed from './components/Feed';
+import SearchResult from './components/SearchResult';
+import VideoDetails from './components/VideoDetails';
 
 const App = () => {
   return (
-    <div className='text-3xl text-green-700 text-center my-4'>Project Setup Completed</div>
+    <>
+      <AppContext>
+      <BrowserRouter>
+                <div className="flex flex-col h-full">
+                    <Header />
+                    <Routes>
+                        <Route path="/" exact element={<Feed />} />
+                        <Route
+                            path="/searchResult/:searchQuery"
+                            element={<SearchResult />}
+                        />
+                        <Route path="/video/:id" element={<VideoDetails />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+      </AppContext>
+    </>
   )
 }
 
